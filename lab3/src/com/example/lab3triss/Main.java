60da249077085cc2edc4db9bdc3b2c3fb15072aa
Triss <vizanttriss@gmail.com>
2019-10-07 15:30:55 +0300

    final version lab 3
    lab 3 full

--- lab3/src/com/example/lab3triss/Main.java
+++ lab3/src/com/example/lab3triss/Main.java
@@ -2,10 +2,9 @@ package com.example.lab3triss;
 
 public class Main {
     public static void main(String[] args) {
-        TextManipulation str = new TextManipulation("Maybsdf sjfbsk bed . ufdsbf euru . bed sfbdf j bsfk sjhbfsey . fsef f e !");
-        //String str = "Maybsdf sjfbsk bed . ufdsbf euru. bed sfbdf j bsfk sjhbfsey. fsef f e!"; //our hardcoded string :)
+        TextManipulation str = new TextManipulation("Maybsdf sjfbsk bed . ufdsbf euru . euru euru bed sfbdf j bsfk sjhbfsey . fsef f e !");
+
         TextManipulation.countSentences();
-        TextManipulation.countWords();
         TextManipulation.countLength();
         TextManipulation.countFrequentWords();
         TextManipulation.countLetters();
--- lab3/src/com/example/lab3triss/TextManipulation.java
+++ lab3/src/com/example/lab3triss/TextManipulation.java
@@ -1,18 +1,16 @@
 package com.example.lab3triss;
 import java.util.*;
-import java.util.Arrays;
 
 public class TextManipulation {
     public static String str;
-    TextManipulation(String str){
-        this.str=str;
+    public static String delim = ".,::?!";
+
+    TextManipulation(String str) {
+        this.str = str.toLowerCase();
     }
 
-    //-------------Sentence counter-------------//
-
     public static void countSentences() {
-        int  sentenceCount=1; //setting counter
-        String delim = ".,::?!"; //setting delimiters
+        int  sentenceCount = 1; //setting counter
         for (int i = 0; i < str.length() - 1; i++) { //a for loop using our vars str and delim
             for (int j = 0; j < delim.length(); j++) {
                 if (str.charAt(i) == delim.charAt(j)) {
@@ -26,48 +24,31 @@ public class TextManipulation {
         System.out.println("Number of sentences: "+ sentenceCount);;
     }
 
-    //---------Getting number of words------------------------//
-
-    public static void countWords() {
-        int words = 1;
-        for (int i = 0; i < str.length() - 1; i++) {
-            if ((str.charAt(i) == ' ') && (str.charAt(i + 1) != ' ')) {
-                words++;
-            }
-        }
-        System.out.println("Number of words: " + words);
-    }
-
-    //-------------Getting the longest word---------------
-    public static void countLength(){System.out.println("Longest word is: " + Arrays.stream(str.split(" ")).max(Comparator.comparingInt(String::length)).orElse(null));}
+    public static void countLength(){System.out.println("Longest word is: " + Arrays.stream(str.split(" ")).
+            max(Comparator.
+                    comparingInt(String::length)).
+            orElse(null));}
     //arraystream is used to get a Sequential Stream from the array passed as the parameter with its elements.
     //split() method breaks a given string around matches of the given regular expression.
     //example of using :: --> Person::getName in this context is shorthand for (Person p) -> p.getName()
     //shortway of doing it using comparators
 
-    //----------------Counting the most frequent word---------------//
-
     public static void countFrequentWords() {
-        String[] words = str.split("\\.");    //Split the word from String
-        int wrc = 1;                            //Variable for getting Repeated word count
+        HashMap<String, Integer> frequencyMap = new HashMap<>();
+        String[] words = str.split("\\P{L}+");  //split the words
+        System.out.println("Number of words: " + words.length);
 
-        for (int i = 0; i < words.length; i++)        //Outer loop for Comparison
-        {
-            for (int j = i + 1; j < words.length; j++)    //Inner loop for Comparison
-            {
-                if (words[i].equals(words[j]))    //Checking for both strings are equal
-                {
-                    String bigwrd = words[i];
-                    wrc = wrc + 1;                //if equal increment the count
-                    words[j] = "0";            //Replace repeated words by zero
-                }
-            }
-            if (words[i] != "0")
-                System.out.println(words[i] + "--" + wrc);    //Printing the word along with count
-            wrc = 1;
+        for (int i = 0; i < words.length; i++) {
+            if (frequencyMap.containsKey(words[i]))
+                frequencyMap.put(words[i], frequencyMap.get(words[i]) + 1);
+            else
+                frequencyMap.put(words[i], 1);
         }
+
+        List<Map.Entry<String, Integer>> results = new ArrayList<>(frequencyMap.entrySet());
+        Collections.sort(results, new ValueComparator());
+        System.out.println("Top 5 words : " + results.subList(0, 5));
     }
-            //-----------Calculating nr of letters----------//
 
     public static void countLetters() {
         int count = 0;
@@ -77,20 +58,32 @@ public class TextManipulation {
         }
         System.out.println("Number of letters: " + count);
     }
-    //---------Calculating nr of vowels and consonants--------//
 
-    public static void countVowsCons(){int vowsc=0;
-    int consc=0;
-    str = str.toLowerCase();//this is used in order to reduce comparisions
+    public static void countVowsCons(){
+        int vowsc=0;
+        int consc=0;
+        str = str.toLowerCase();//this is used in order to reduce comparisions
         for(int i = 0; i < str.length(); i++) {//Checks whether a character is a vowel
-        if(str.charAt(i)=='a'||str.charAt(i)=='e'||str.charAt(i)=='i'||str.charAt(i)=='o'||str.charAt(i)=='u'){
-            vowsc++;//Increments the vowel counter
+            if(str.charAt(i)=='a'||str.charAt(i)=='e'||str.charAt(i)=='i'||str.charAt(i)=='o'||str.charAt(i)=='u'){
+                vowsc++;//Increments the vowel counter
+            }
+            else if(str.charAt(i) >= 'a' && str.charAt(i)<='z') {//else its a consonant
+                consc++;//Increments the consonant counter
+            }
         }
-        else if(str.charAt(i) >= 'a' && str.charAt(i)<='z') {//else its a consonant
-            consc++;//Increments the consonant counter
-        }
-    }
         System.out.println("Number of vowels: " + vowsc);
         System.out.println("Number of consonants: " + consc);
+    }
 }
+
+class ValueComparator implements Comparator<Map.Entry<String, Integer>> {
+
+    @Override
+    public int compare(Map.Entry<String, Integer> first, Map.Entry<String, Integer> second) {
+        if(first.getValue() > second.getValue())
+            return -1;
+        else if (first.getValue() < second.getValue())
+            return 1;
+        return 0;
+    }
 }
